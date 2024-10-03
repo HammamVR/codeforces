2451745ca4f2ea6d811f6f6e769b9251c85b133b
Hammam <hammamvr.alfarouq@gmail.com>
2024-10-03 09:20:27 +0300

    Update Range sum.cpp

--- Range sum.cpp
+++ Range sum.cpp
@@ -1,3 +1,24 @@
+#include<bits/stdc++.h>
+#define hammam ios_base::sync_with_stdio(NULL); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main() {
+    hammam
+        int t ; cin >> t;
+        while (t--) {
+            long long l , r ; cin >> l >> r;
+            if (l > r) swap (l,r);
+            long long L = l-1;
+            long long x = L*(L+1)/2, y = r*(r+1)/2;
+            cout << y-x <<endl;
+        }
+        return 0;
+    }
+
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
